c3ad8fede9dd34b053cccfb44ff16ac4a2aaf750
Gaurav Sharma <gshandilya0001@gmail.com>
2024-05-31 23:14:44 +0530

    Add files via upload

---
+++ pizza_sales.sql
@@ -0,0 +1,8 @@
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+
+select * from order_details;
+select* from orders;
+select * from pizza_types;
+select * from pizzas;
+
+

feec0aca3c0c3974e0768cef917150e6942bf7be
Gaurav Sharma <gshandilya0001@gmail.com>
2024-06-01 17:21:48 +0530

    Delete pizza_sales.sql

--- pizza_sales.sql
+++
@@ -1,8 +0,0 @@
--- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
-
-select * from order_details;
-select* from orders;
-select * from pizza_types;
-select * from pizzas;
-
-
